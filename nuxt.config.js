9c6cbf560306dbc296926e814302b88791f0dafa
happi2206 <happinessadeboye@gmail.com>
2022-04-29 13:02:24 +0100

    build: :building_construction:

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
-  target: "static",
+  // target: "static",
   // router: {
   //   base: "/health-med/",
   // },
